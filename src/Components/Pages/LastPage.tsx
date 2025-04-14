c46d8dff494ffa0defd43060a16ed5211d5f2541
saulizar93 <saulojeda@ymail.com>
2025-04-13 17:48:54 -0700

    add asset files

---
+++ src/Components/Pages/LastPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LastPage.css';
+
+const LastPage = () => {
+  return (
+    <div className="book-page">
+      <div className="last-page-container"></div>
+    </div>
+  )
+}
+
+
+export default LastPage;
---
+++ src/Components/Pages/LuzPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './LuzPage.css';
+
+const LuzPage = () => {
+  return (
+    <div className="book-page">
+      <div className="luz-container">
+        <div className="luz-message">
+          <p>
+            Nos conocemos desde que eramos adolescentes y cada uno tomó un camino distinto, sin embargo Dios 
+            creó un amor tan fuerte que no desaparece ante cualquier 
+            circunstancia lo cual comprobamos.
+          </p>
+          <p>
+            Te elegí con determinación para acompañarnos 
+            y caminar juntos en esta vida.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default LuzPage;
